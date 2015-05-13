9a174cb563f78c214b250ec38606cdc1cd07c1bd
Jonathan Hartman <jhartman86@gmail.com>
2015-05-12 20:54:17 -0600

    Updates

--- web/packages/artsy/js/app.js
+++ web/packages/artsy/js/app.js
@@ -30,109 +30,8 @@
     });
 
 })(window, window.angular);
-angular.module('artsy.common', []);
 angular.module('artsy.elements', []);
-/* global Modernizr */
-/* global FastClick */
-angular.module('artsy.common').
-
-    /**
-     * @description Modernizr provider
-     * @param $window
-     * @param $log
-     * @returns Modernizr | false
-     */
-    provider('Modernizr', function(){
-        this.$get = ['$window', '$log',
-            function( $window, $log ){
-                return $window['Modernizr'] || ($log.warn('Modernizr unavailable!'), false);
-            }
-        ];
-    }).
-
-    /**
-     * @description TweenLite OR TweenMax provider
-     * @param $window
-     * @param $log
-     * @returns TweenMax | TweenLite | false
-     */
-    provider('Tween', function(){
-        this.$get = ['$window', '$log',
-            function( $window, $log ){
-                return $window['TweenMax'] || $window['TweenLite'] || ($log.warn('Tween library unavailable!'), false);
-            }
-        ];
-    }).
-
-    /**
-     * @description MomentJS
-     * @param $window
-     * @param $log
-     * @returns moment | TweenLite | false
-     */
-    provider('moment', function(){
-        this.$get = ['$window', '$log',
-            function( $window, $log ){
-                return $window['moment'] || ($log.warn('MomentJS library unavailable!'), false);
-            }
-        ];
-    }).
-
-    /**
-     * @description Isotope provider
-     * @param $window
-     * @param $log
-     * @returns Isotope | false
-     */
-    provider('Isotope', function(){
-        this.$get = ['$window', '$log',
-            function( $window, $log ){
-                return $window['Isotope'] || ($log.warn('Isotope unavailable!'), false);
-            }
-        ];
-    }).
-
-    /**
-     * @description imagesLoaded provider
-     * @param $window
-     * @param $log
-     * @returns imagesLoaded | false
-     */
-    provider('imagesLoaded', function(){
-        this.$get = ['$window', '$log',
-            function( $window, $log ){
-                return $window['imagesLoaded'] || ($log.warn('imagesLoaded unavailable!'), false);
-            }
-        ];
-    }).
-
-    /**
-     * @description FastClick provider
-     * @param $window
-     * @param $log
-     * @returns FastClick | false
-     */
-    provider('FastClick', function(){
-        this.$get = ['$window', '$log',
-            function( $window, $log ){
-                return $window['FastClick'] || ($log.warn('FastClick unavailable!'), false);
-            }
-        ];
-    }).
-
-    /**
-     * @description svg.js provider
-     * @param $window
-     * @param $log
-     * @returns SVG | false
-     */
-    provider('SVG', function(){
-        this.$get = ['$window', '$log',
-            function( $window, $log ){
-                return $window['SVG'] || ($log.warn('SVG.js unavailable!'), false);
-            }
-        ];
-    });
+angular.module('artsy.common', []);
 angular.module('artsy.common').
 
     directive('eventCalendar', ['$http', 'moment', function( $http, moment ){
@@ -188,7 +87,6 @@ angular.module('artsy.common').
                     'calendarID',
                     'title',
                     'description',
-                    'isSynthetic',
                     'computedStartUTC',
                     'computedStartLocal'
                 ];
@@ -201,7 +99,9 @@ angular.module('artsy.common').
                     tags:      null,
                     category:  null,
                     filepath:  true,
-                    pagepath:  true
+                    pagepath:  true,
+                    grouping:  true,
+                    end:       moment().add(7, 'days').format('YYYY-MM-DD')
                 };
 
                 /**
@@ -386,3 +286,104 @@ angular.module('artsy.common').
             link: _link
         };
     }]);
+/* global Modernizr */
+/* global FastClick */
+angular.module('artsy.common').
+
+    /**
+     * @description Modernizr provider
+     * @param $window
+     * @param $log
+     * @returns Modernizr | false
+     */
+    provider('Modernizr', function(){
+        this.$get = ['$window', '$log',
+            function( $window, $log ){
+                return $window['Modernizr'] || ($log.warn('Modernizr unavailable!'), false);
+            }
+        ];
+    }).
+
+    /**
+     * @description TweenLite OR TweenMax provider
+     * @param $window
+     * @param $log
+     * @returns TweenMax | TweenLite | false
+     */
+    provider('Tween', function(){
+        this.$get = ['$window', '$log',
+            function( $window, $log ){
+                return $window['TweenMax'] || $window['TweenLite'] || ($log.warn('Tween library unavailable!'), false);
+            }
+        ];
+    }).
+
+    /**
+     * @description MomentJS
+     * @param $window
+     * @param $log
+     * @returns moment | TweenLite | false
+     */
+    provider('moment', function(){
+        this.$get = ['$window', '$log',
+            function( $window, $log ){
+                return $window['moment'] || ($log.warn('MomentJS library unavailable!'), false);
+            }
+        ];
+    }).
+
+    /**
+     * @description Isotope provider
+     * @param $window
+     * @param $log
+     * @returns Isotope | false
+     */
+    provider('Isotope', function(){
+        this.$get = ['$window', '$log',
+            function( $window, $log ){
+                return $window['Isotope'] || ($log.warn('Isotope unavailable!'), false);
+            }
+        ];
+    }).
+
+    /**
+     * @description imagesLoaded provider
+     * @param $window
+     * @param $log
+     * @returns imagesLoaded | false
+     */
+    provider('imagesLoaded', function(){
+        this.$get = ['$window', '$log',
+            function( $window, $log ){
+                return $window['imagesLoaded'] || ($log.warn('imagesLoaded unavailable!'), false);
+            }
+        ];
+    }).
+
+    /**
+     * @description FastClick provider
+     * @param $window
+     * @param $log
+     * @returns FastClick | false
+     */
+    provider('FastClick', function(){
+        this.$get = ['$window', '$log',
+            function( $window, $log ){
+                return $window['FastClick'] || ($log.warn('FastClick unavailable!'), false);
+            }
+        ];
+    }).
+
+    /**
+     * @description svg.js provider
+     * @param $window
+     * @param $log
+     * @returns SVG | false
+     */
+    provider('SVG', function(){
+        this.$get = ['$window', '$log',
+            function( $window, $log ){
+                return $window['SVG'] || ($log.warn('SVG.js unavailable!'), false);
+            }
+        ];
+    });
