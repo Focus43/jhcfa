75a2779ec34ca3a3ddb57d34da31aa8094e5221f
Jonathan Hartman <jhartman86@gmail.com>
2015-05-13 16:35:11 -0600

    Thumbnails for staff page

--- web/packages/artsy/blocks/photo_wall/templates/staff.php
+++ web/packages/artsy/blocks/photo_wall/templates/staff.php
@@ -1,6 +1,6 @@
 <div class="staff-list">
     <?php foreach((array)$fileListResults AS $fileObj): ?>
-        <div class="staff-member" style="background-image:url('<?php echo $fileObj->getRelativePath(); ?>');">
+        <div class="staff-member" style="background-image:url('<?php echo $fileObj->getThumbnailURL('event_thumb'); ?>');">
             <span class="details">
                 <span class="name"><?php echo $fileObj->getTitle(); ?></span>
                 <span class="descr"><?php echo $fileObj->getDescription(); ?></span>
