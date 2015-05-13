16a9c35a73f09ae7be3260aeacd86c9e157c0ff1
Jonathan Hartman <jhartman86@gmail.com>
2015-05-13 15:58:38 -0600

    Staff image gallery block

---
+++ web/packages/artsy/blocks/photo_wall/templates/staff.php
@@ -0,0 +1,11 @@
+<div class="staff-list">
+    <?php foreach((array)$fileListResults AS $fileObj): ?>
+        <div class="staff-member" style="background-image:url('<?php echo $fileObj->getRelativePath(); ?>');">
+            <span class="details">
+                <span class="name"><?php echo $fileObj->getTitle(); ?></span>
+                <span class="descr"><?php echo $fileObj->getDescription(); ?></span>
+            </span>
+            <a class="mail" href="mailto:loremipsum@test.com"><i class="icon-mail2"></i></a>
+        </div>
+    <?php endforeach; ?>
+</div>
