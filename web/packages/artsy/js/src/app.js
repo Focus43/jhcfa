f9a6fd44d9a6f634776b6123f4694a9fe8d36656
Jonathan Hartman <jhartman86@gmail.com>
2015-05-27 17:03:44 -0600

    Update

--- web/packages/artsy/js/src/app.js
+++ web/packages/artsy/js/src/app.js
@@ -23,38 +23,19 @@
         function( FastClick ){
             FastClick.attach(document.body);
 
-            //var themeWraps = document.querySelectorAll('[class*="wrap-theme-"]');
-            //if( themeWraps.length ){
-            //    for(var i = 0, len = themeWraps.length; i < len; i++){
-            //        var $element = angular.element(themeWraps[i]);
-            //        if( $element.length ){
-            //
-            //        }
-            //        while( ! $element.hasClass('container') ){
-            //            $element = angular.element($element.parent());
-            //        }
-            //        if( $element.length ){
-            //            $element.addClass('overflowable-x');
-            //        }
-            //    }
-            //}
-
-            //if( themeWraps.length ){
-            //    for(var i = 0, len = themeWraps.length; i < len; i++){
-            //        var containerEl = themeWraps[i];
-            //        if( containerEl ){
-            //            var cl = containerEl.classList;
-            //            if( cl ){
-            //                while( ! cl.contains('container') ){
-            //                    containerEl = containerEl.parentNode;
-            //                }
-            //                if( containerEl ){
-            //                    containerEl.classList.add('overflowable-x');
-            //                }
-            //            }
-            //        }
-            //    }
-            //}
+            var themeWraps = document.querySelectorAll('[class*="wrap-theme-"]');
+            if( themeWraps.length ){
+                for(var i = 0, len = themeWraps.length; i < len; i++){
+                    var $element = angular.element(themeWraps[i]);
+                    while( ! $element.hasClass('container') && $element.length ){
+                        console.log('calling');
+                        $element = angular.element($element.parent());
+                    }
+                    if( $element.length ){
+                        $element.addClass('overflowable-x');
+                    }
+                }
+            }
         }
     ]);
 
