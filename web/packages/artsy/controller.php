6ad897d071862ab4c590d5e38e3a78fa337286ea
Jonathan Hartman <jhartman86@gmail.com>
2015-06-08 11:07:37 -0600

    updates

--- web/packages/artsy/controller.php
+++ web/packages/artsy/controller.php
@@ -39,7 +39,7 @@
 
         protected $pkgHandle 			        = self::PACKAGE_HANDLE;
         protected $appVersionRequired 	        = '5.7.3.2';
-        protected $pkgVersion 			        = '0.28';
+        protected $pkgVersion 			        = '0.29';
 
 
         /**
