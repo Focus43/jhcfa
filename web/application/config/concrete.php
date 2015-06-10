a1a25b02bc5c6fdb3203d6b0042fb131f7ac11f7
Jonathan Hartman <jhartman86@gmail.com>
2015-06-10 07:58:33 -0600

    tracking code

--- web/application/config/concrete.php
+++ web/application/config/concrete.php
@@ -17,12 +17,28 @@ $redisStashCacheDriver = array(
     )
 );
 
+$trackingCode = <<<EOT
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-21129410-1', 'auto');
+  ga('send', 'pageview');
+</script>
+EOT;
+
+
 return array(
     'maintenance_mode' => false,
     'site' => 'Center for the Arts',
     'seo' => array(
         'url_rewriting' => true,
-        'url_rewriting_all' => true
+        'url_rewriting_all' => true,
+        'tracking' => array(
+            'code' => $trackingCode
+        )
     ),
     'sitemap_xml' => array(
         'file' => 'application/files/sitemap.xml',
