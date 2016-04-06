7f286db813ae0016d5c8658bb4039aa401e7e002
Arik Griffin <arik@focus-43.com>
2016-04-06 13:17:43 -0600

    rollback of event.php to original state

--- web/packages/artsy/themes/artsy/event.php
+++ web/packages/artsy/themes/artsy/event.php
@@ -2,9 +2,6 @@
 <html lang="<?php echo LANGUAGE; ?>" class="<?php echo $documentClasses; ?>">
 <?php $this->inc('elements/head.php'); ?>
 
-<?php $this->inc()'/elements/json-ld.php'); ?>
-<script type="application/ld+json"><?php echo json_encode($payload); ?></script>
-
 <body class="<?php echo $templateHandle; ?>">
 
 <div id="c-level-1" class="<?php echo $c->getPageWrapperClass(); ?>">
