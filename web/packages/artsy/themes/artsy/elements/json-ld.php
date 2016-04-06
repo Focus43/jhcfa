14c506566e42fdf459e7eb046f132b5853bb34a5
Arik Griffin <arik@focus-43.com>
2016-04-06 13:42:40 -0600

    json payload is starting to deliver to page without breaking. added event type

--- web/packages/artsy/themes/artsy/elements/json-ld.php
+++ web/packages/artsy/themes/artsy/elements/json-ld.php
@@ -7,6 +7,7 @@
 
 // stuff for any page
 $payload["@context"] = "http://schema.org/";
+$payload["@type"] = "Event";
 
 // this has all the data of the post/page etc
 // $post_data = get_post_data();
