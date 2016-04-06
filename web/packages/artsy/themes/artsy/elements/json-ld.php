fe9f09347c49f8df14ffdfa85c37503fa9c7085b
Arik Griffin <arik@focus-43.com>
2016-04-06 14:27:28 -0600

    pull out auto load of name to see if that's the problem

--- web/packages/artsy/themes/artsy/elements/json-ld.php
+++ web/packages/artsy/themes/artsy/elements/json-ld.php
@@ -9,7 +9,7 @@
 // stuff for any page
   $payload["@context"] = "http://schema.org/";
     $payload["@type"] = "Event";
-      $payload["@Name"] = echo $eventObj->getAttribute('presenting_organization');
+      $payload["@Name"] = "this the name of the event";
 
 // this has all the data of the post/page etc
 // $post_data = get_post_data();
