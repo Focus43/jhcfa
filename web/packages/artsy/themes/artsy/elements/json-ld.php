36b79e8a62756612d9e7533c1538e9b16956a50a
Arik Griffin <arik@focus-43.com>
2016-04-07 14:12:01 -0600

    revert back to most basic json-ld php

--- web/packages/artsy/themes/artsy/elements/json-ld.php
+++ web/packages/artsy/themes/artsy/elements/json-ld.php
@@ -1,16 +1,8 @@
 <?php
 
-function get_eventObject() {
-//  global $post;
-//  return $post;
-}
-
-$eventObject = get_eventObject();
-
-
   $payload["@context"] = "http://schema.org/";
     $payload["@type"] = "Event";
-      $payload["name"] = $eventObject  //"name of the event"; // need to pull this from actual event
+      $payload["name"] = "name of the event"; // need to pull this from actual event
       $payload["startDate"] = "2016-05-14T21:30"; // need to pull this from actual event
 
       $payload["location"] = array(
