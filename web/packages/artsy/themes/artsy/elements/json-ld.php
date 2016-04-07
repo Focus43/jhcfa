f3aaecf7b0aed9fac6ed1d1a1a3ee3ced2d984bc
Arik Griffin <arik@focus-43.com>
2016-04-07 12:45:17 -0600

    correction of Thing type to Place

--- web/packages/artsy/themes/artsy/elements/json-ld.php
+++ web/packages/artsy/themes/artsy/elements/json-ld.php
@@ -14,7 +14,7 @@
       $payload["startDate"] = "2016-05-14T21:30"; // need to pull this from actual event
 
       $payload["location"] = array(
-        "@type" => "location",
+        "@type" => "Place",
         "sameAs" => "http://jhcenterforthearts.org/",
         "name" => "The Center",
         "address" => "240 S. Glenwood St., Jackson, WY 83001",
