72273c13ab1b59c016e925e54060465ec5b55444
Arik Griffin <arik@focus-43.com>
2016-04-08 12:41:06 -0600

    mistake corrected - added gmwt verification to head.php

--- web/packages/artsy/themes/artsy/elements/head.php
+++ web/packages/artsy/themes/artsy/elements/head.php
@@ -4,6 +4,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=EDGE" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, minimal-ui">
 <meta name="apple-mobile-web-app-capable" content="no" />
+<meta name="google-site-verification" content="CBBLQn2GZkXqX0QOx3KvR6w5UhxzbT-jdVQZAmbUJdo" />
 <?php Loader::element('header_required'); // REQUIRED BY C5 // ?>
 <?php
     if( ! $this->controller instanceof \Concrete\Package\Artsy\Controller\ArtsyPageController ){
