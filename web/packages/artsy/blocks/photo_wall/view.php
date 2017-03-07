d45e28fb5535f34fb9c96376d892010ce2912190
Sam Fitz <abigwonderful@gmail.com>
2017-03-06 21:50:53 -0700

    new check for image links added

--- web/packages/artsy/blocks/photo_wall/view.php
+++ web/packages/artsy/blocks/photo_wall/view.php
@@ -1,14 +1,19 @@
 <div class="aligned-grid">
-    <?php foreach((array)$fileListResults AS $fileObj){ if( is_object($fileObj) ){
-        $imgPath = $fileObj->getThumbnailURL('file_manager_detail');
-        $pageObj = Page::getByID($fileObj->getAttribute('link'));
-        $link = '';
-        if( is_object($pageObj) && $pageObj->getCollectionID() >= 1 ){
-            $link = sprintf('href="%s"', $pageObj->getCollectionPath());
-        }
-        ?>
-        <a class="item" <?php echo $link; ?> style="background-image:url('<?php echo $imgPath; ?>');">
-            <img src="<?php echo $imgPath; ?>" />
-        </a>
+    <?php foreach( (array)$fileListResults AS $fileObj ){
+        if( is_object($fileObj) ){
+            $imgPath = $fileObj->getThumbnailURL('file_manager_detail');
+            $pageObj = Page::getByID( $fileObj->getAttribute('link') );
+            // var_dump( $pageObj );
+            $link = '';
+            if( is_object($pageObj) && $pageObj->getCollectionID() >= 1 ){
+                $link = sprintf('href="%s"', $pageObj->getCollectionPath());
+            }
+            if( is_object($pageObj) && $pageObj->isExternalLink() ){
+                $link = sprintf('href="%s"', $pageObj->getCollectionPointerExternalLink());
+            }
+    ?>
+            <a class="item" <?php echo $link; ?> style="background-image:url('<?php echo $imgPath; ?>');">
+                <img src="<?php echo $imgPath; ?>" />
+            </a>
     <?php } } ?>
 </div>
